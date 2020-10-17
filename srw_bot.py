7b133013ded39160532268af212d8d1768704caa
dichka <salkov.x.danil.99@gmail.com>
2020-10-17 21:47:36 +0300

    feature: created config file with API-TOKEN

--- srw_bot.py
+++ srw_bot.py
@@ -1,9 +1,10 @@
-
 import telebot
+
 # import json
 
-bot = telebot.TeleBot('841729239:AAG9QCGYlTReRnwVgYOuLY6JwN6c5ZAN-HM')
+API_TOKEN = open('config.txt')
 
+bot = telebot.TeleBot(API_TOKEN.read())
 
 keyboard1 = telebot.types.ReplyKeyboardMarkup(True)
 keyboard1.row('Привет', 'Пока')
@@ -12,7 +13,8 @@ keyboard1.row('Привет', 'Пока')
 @bot.message_handler(commands=['start'])
 def start_message(message):
     hello = bot.send_message(message.chat.id, text=' Привет, ' + message.from_user.first_name + '\nты написал мне '
-                                                                                                '/start', reply_markup=keyboard1)
+                                                                                                '/start',
+                             reply_markup=keyboard1)
     return print(hello)
 
 
@@ -22,7 +24,6 @@ def send_text(message):
         bot.send_message(message.chat.id, 'Привет, пользователь')
     elif message.text.lower() == 'пока':
         bot.send_message(message.chat.id, 'До скорой встречи')
-
 
 
 bot.polling()
