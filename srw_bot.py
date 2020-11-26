644c55af669b76c81d6d0601d1e68d4c334a2dbe
dichka <salkov.x.danil.99@gmail.com>
2020-11-27 01:58:58 +0300

    feature: add text classification

--- srw_bot.py
+++ srw_bot.py
@@ -1,6 +1,10 @@
 import telebot
+import pickle
+from nltk.stem import PorterStemmer as ps
+import re
+from nltk.corpus import stopwords
 
-# import json
+stemmer = ps()
 
 API_TOKEN = open('config.txt')
 
@@ -8,6 +12,15 @@ bot = telebot.TeleBot(API_TOKEN.read())
 
 keyboard1 = telebot.types.ReplyKeyboardMarkup(True)
 keyboard1.row('Привет', 'Пока')
+
+
+with open('dat.pickle', 'rb') as f:
+    corpus = pickle.load(f)
+    uniqueWords = pickle.load(f)
+
+with open('mod.pickle', 'rb') as f:
+    classifier_mnb = pickle.load(f)
+    cv = pickle.load(f)
 
 
 @bot.message_handler(commands=['start'])
@@ -24,6 +37,20 @@ def send_text(message):
         bot.send_message(message.chat.id, 'Привет, пользователь')
     elif message.text.lower() == 'пока':
         bot.send_message(message.chat.id, 'До скорой встречи')
+    else:
+        corpus_bot = list()
+        text = re.sub("[^a-zA-Z]", ' ', message.text)
+        text = text.lower()
+        text = text.split()
+        text = [stemmer.stem(word) for word in text if not word in set(stopwords.words('english'))]
+        text = ' '.join(text)
+        corpus_bot.append(text)
+        print(corpus_bot)
+        test = cv.transform(corpus_bot).todense()
+        if classifier_mnb.predict(test) == 1:
+            bot.send_message(message.chat.id, 'Правда')
+        else:
+            bot.send_message(message.chat.id, 'Ложь')
 
 
 bot.polling()
